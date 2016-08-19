35209ce68080e116bd62a9d5e2b31aacfe2cd96f
nopony <bc.biran@gamil.com>
2016-08-19 22:50:48 +0200

    sorted files to support static routing ('public' folder) added a favicon fix for firefox

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 /**
  * Created by nopony on 19/08/2016.
- */ var express = require('express')
- var app = express();
- var bodyparser = require('body-parser');
-var fs = require('fs')
- var PORT = 80; 
+ */
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var PORT = 80;
 
-app.use(bodyparser.urlencoded({ extended: false })); 
-app.use(express.static('.')); 
-app.use(bodyparser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('.'));
+app.use(bodyParser.json());
 app.post('/subscribe', function(req, res) {
-	console.log('received a sub request, processing...')
+	console.log('received a sub request, processing...');
 	fs.appendFileSync('mails.txt', req.body.email + '\n');
 	console.log('done!')
 });
 app.post('/message', function(req, res) {
-	console.log('received message from user\n')
-	fs.appendFile('messages.txt', req.body.contact_name + '\n'
-					+ req.body.contact_email + '\n'
-					+ req.body.contact_message + '\n', function(err) {
-	if(err) console.log(err);
-	else console.log('done!');
+	console.log('received message from user\n');
+	fs.appendFile('messages.txt',
+		req.body.contact_name + '\n'
+		+ req.body.contact_email + '\n'
+		+ req.body.contact_message + '\n', function(err) {
+	        if(err) console.log(err);
+	        else console.log('done!');
 	});
-})
+});
 
 app.listen(PORT, function() {
    console.log('listening on port ' + PORT);
