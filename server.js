e6abaaa5649f21ead8da2f8ac541555499c94d23
nopony <bc.biran@gamil.com>
2016-08-22 12:30:52 +0200

    added a toast confirming form submissions

--- server.js
+++ server.js
@@ -11,22 +11,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.post('/subscribe', function(req, res) {
-	console.log('received a sub request, processing...');
-	fs.appendFileSync('mails.txt', req.body.email + '\n');
-	console.log('done!')
+    console.log('received a sub request, processing...');
+    fs.appendFile('mails.txt', req.body.email + '\n', function (err) {
+        if(err) console.log(err);
+        else res.send({saved: 0});
+    });
+    console.log('done, responded with code 0')
+
 });
 app.post('/message', function(req, res) {
-	console.log('received message from user\n');
-	fs.appendFile('messages.txt',
-		req.body.contact_name + '\n'
-		+ req.body.contact_email + '\n'
-		+ req.body.contact_message + '\n', function(err) {
-	        if(err) console.log(err);
-	        else console.log('done!');
-	});
+    console.log('received message from user\n');
+    fs.appendFile('messages.txt',
+        req.body.contact_name + '\n'
+        + req.body.contact_email + '\n'
+        + req.body.contact_message + '\n', function(err) {
+            if(err) console.log(err);
+
+            else {
+                res.send({saved: 0});
+                console.log('done, responded with code 0');
+            }
+        });
 });
 
 app.listen(PORT, function() {
-   console.log('listening on port ' + PORT);
+    console.log('listening on port ' + PORT);
 });
 
